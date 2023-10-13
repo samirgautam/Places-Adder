5ea5a8b0c1d854cf1c6623f2bbad20d02c3d6478
Samir Gautam <samirgautam003@gmail.com>
2023-10-13 13:40:06 +0545

    updated the add new place component features with validation

--- FRONTEND/src/places/pages/NewPlace.js
+++ FRONTEND/src/places/pages/NewPlace.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/utils/validators";
 import './Newplace.css';
 
 const NewPlace = () => {
     return <form className="place-form">
-        <Input type = "text" label = "Title" element = "element" />
+        <Input element="input" type = "text" label = "Title" validators = {[VALIDATOR_REQUIRE()]} errorText = "Please enter a valid text" />
     </form>;
 };
 
