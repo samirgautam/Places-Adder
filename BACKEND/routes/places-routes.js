511080822d68104347911a8d61522e0807a792e4
Samir Gautam <samirgautam003@gmail.com>
2024-01-14 19:18:16 +0545

    own error model with HttpError class

--- BACKEND/routes/places-routes.js
+++ BACKEND/routes/places-routes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
+
 
 const DUMMY_PLACES = [
   {
@@ -24,9 +27,7 @@ router.get("/:pid", (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error("Could not find a place for the provided id.");
-    error.code = 404;
-    throw error;
+    throw new HttpError("Could not find a place for the provided id.", 404);
   }
 
   res.json({ place });
@@ -40,9 +41,7 @@ router.get("/user/:uid", (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error("Could not find a place for the provided user id.");
-    error.code = 404;
-    return next(error);
+    return next(new HttpError("Could not find a place for the provided user id.", 404));
   }
   res.json({ place });
 });
