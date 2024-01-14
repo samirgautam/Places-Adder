7171db0a95f02a9f5865d02aa61e9a932ee18b35
Samir Gautam <samirgautam003@gmail.com>
2024-01-14 08:31:42 +0545

    place specific route

--- BACKEND/routes/places-routes.js
+++ BACKEND/routes/places-routes.js
@@ -2,9 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next)=> {
-    console.log('GET request in places');
-    res.json({message: "It Works !"});
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Dharahara Tower',
+        description: 'One of the beautiful landmarks',
+        location: {
+            lat : 27.7004757,
+            lng : 85.312657,
+        },
+        address : 'Sundhara Rd, Kathmandu',
+        creator : 'u1'
+    }
+];
+
+router.get('/:pid', (req, res, next) => {
+    const placeId = req.params.pid;
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placeId;
+    });
+    res.json({place});
 });
 
 module.exports = router;
