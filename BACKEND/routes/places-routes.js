2319650d466c40e35215bcbfe6188f149c3abea6
Samir Gautam <samirgautam003@gmail.com>
2024-01-14 08:56:47 +0545

    route that handles users

--- BACKEND/routes/places-routes.js
+++ BACKEND/routes/places-routes.js
@@ -18,10 +18,22 @@ const DUMMY_PLACES = [
 
 router.get('/:pid', (req, res, next) => {
     const placeId = req.params.pid;
+
     const place = DUMMY_PLACES.find(p => {
         return p.id === placeId;
     });
+
     res.json({place});
 });
+
+router.get('/user/:uid' , (req,res,next) => {
+    const userId = req.params.uid;
+    
+    const place = DUMMY_PLACES.find(p=> {
+        return p.creator === userId;
+    });
+    
+    res.json({place});
+})
 
 module.exports = router;
