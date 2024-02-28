8e7f8867a5ab9534820af08968965ecce0706b4d
Samir Gautam <samirgautam003@gmail.com>
2024-02-28 17:13:12 +0545

    added deletePLace

--- BACKEND/controllers/places-controller.js
+++ BACKEND/controllers/places-controller.js
@@ -4,7 +4,7 @@ const HttpError = require("../models/http-error");
 
 
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: "p1",
     title: "Dharahara Tower",
@@ -79,7 +79,11 @@ const updatePlace = (req, res, next) => {
   res.status(200).json({place : updatedPlace});
 };
 
-const deletePlace = (req,res, next) => {};
+const deletePlace = (req,res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
+  res.status(200).json({message : 'Deleted Place. '});
+};
 
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
