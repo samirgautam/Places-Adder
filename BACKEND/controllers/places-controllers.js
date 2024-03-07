ac03368b3f41d9543160430a0dbc8171252f7c21
Samir Gautam <samirgautam003@gmail.com>
2024-03-07 18:39:45 +0545

    updated getPlaceById and getPlaceByUserId methods to get places from db using mongoose

--- BACKEND/controllers/places-controllers.js
+++ BACKEND/controllers/places-controllers.js
@@ -20,35 +20,41 @@ let DUMMY_PLACES = [
   },
 ];
 
-const getPlaceById = (req, res, next) => {
+const getPlaceById =async (req, res, next) => {
   const placeId = req.params.pid;
-
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
+let place; 
+  try{
+     place =await Place.findById(placeId);
+  }catch(err){
+    const error = new HttpError('Something went wrong, could not find a place',500);
+    return next(error);
+  };
 
   if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404);
+    const error = HttpError("Could not find a place for the provided id.", 404);
+    return next(error);
   }
-
-  res.json({ place });
+  res.json({ place : place.toObject( {getters: true}) });
 };
 
 //function getPlaceById(){...}
 
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-
-  const places = DUMMY_PLACES.filter((p) => {
-    return p.creator === userId;
-  });
+ let places;
+  try{
+     places = await Place.find({creator : userId});
+  } catch(err) {
+   const error =  new HttpError("Fetching places failed, please try again later",500);
+    return next(error);
+  }
 
   if (!places || places.length === 0) {
     return next(
       new HttpError("Could not find a place for the provided user id.", 404)
     );
   }
-  res.json({ places });
+  res.json({ places : places.map(place => place.toObject({getters : true })) });
 };
 
 const createPlace = async (req, res, next) => {
@@ -60,7 +66,7 @@ const createPlace = async (req, res, next) => {
 
  }
 
-  const {title , description, address , creator } = req.body;
+const {title , description, address , creator } = req.body;
  let coordinates;
  try {
   coordinates = await getCoordsForAddress(address);
